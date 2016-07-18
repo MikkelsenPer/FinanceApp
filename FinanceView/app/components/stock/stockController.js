dd3966b7c063403739637d3e5143961888cd200f
pem <PEM@nykredit.dk>
2016-07-18 16:03:45 +0200

    updated code - calls yql to get index and stock data

--- FinanceView/app/components/stock/stockController.js
+++ FinanceView/app/components/stock/stockController.js
@@ -1,24 +1,30 @@
 //This is the controller for a specific stock.
 
   angular.module('stockModule', [])
-  .controller('stockctrl', ['$http', '$scope','StockResourceHTTP', 'StockIndexResource', stock_ctrl]);
+  .controller('stockctrl', ['$http', '$scope','StockResourceHTTP', 'StockIndexResource', 'StockGlobalIndexResource', stock_ctrl]);
 
 
 
-  function stock_ctrl($http, $scope, StockResourceHTTP, StockIndexResource) {
+  function stock_ctrl($http, $scope, StockResourceHTTP, StockIndexResource, StockGlobalIndexResource) {
       var vm = this;  		
       
      
-      vm.getStock = function() {
-          vm.name = $scope.search_stock;
-          vm.details = StockResourceHTTP.getDetails();
-    }
+      vm.getStock = function($http){
+        vm.name = $scope.search_stock;
+        StockResourceHTTP.setStock($scope.search_stock);  
+        vm.details = StockResourceHTTP.getStockDetailsYql();
+          
+      }
       
     vm.getStockIndex = function() {
-          vm.index = StockIndexResource.getIndex();
-          
-          vm.done = "true";
+          vm.index = StockIndexResource.getIndex();        
       }
+    
+    vm.getGlobalIndexes = function() {
+        vm.globalIndexes = StockGlobalIndexResource.getGlobalIndexes();
+        vm.done = "true";
+        
+    }
     
 	};
 
--- FinanceView/app/components/stock/stockService.js
+++ FinanceView/app/components/stock/stockService.js
@@ -1,41 +1,63 @@
 'use strict';
-/* 
-App.factory('StockResourceHTTP', ['$http', function($http) {   
-    var obj = {content:null};
-    var stockQuery = "http://finance.yahoo.com/webservice/v1/symbols/"
-    
-    $http.get(stockQuery +"MSFT" +"/quote?format=json")
-    .success(function(data, status, headers, config) {               
-        obj.name = data.list.resources[0].resource.fields.name;
-        obj.price = data.list.resources[0].resource.fields.price;
-        obj.volume = data.list.resources[0].resource.fields.volume;
-       
-      })
-    .error(function(data, status, headers, config) {
-        obj.content = data;                                                        
-     });
-    return obj
-    }]);
-    */
 
-App.factory('StockResourceHTTP', function($http){
-    var obj = {content:null};
-    var stockQuery = "http://finance.yahoo.com/webservice/v1/symbols/"
+App.factory('StockResourceHTTP', function($http) {    
+    var service = {content:null};
+    var stockQuery = "http://finance.yahoo.com/webservice/v1/symbols/" 
+    var stockQueryYql = "select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22"
+    var stockName = '';
+    var vm = this;
     
-    //get jsonp
-   return{
-       getDetails: function($scope) {
-        $http.get(stockQuery +"MSFT" +"/quote?callback=JSON_CALLBACK")
-       
-    //    $http.jsonp("http://test.uxco.co/json-test.php?callback=JSON_CALLBACK")
-        .success(function(data, status, headers, config) {               
-        obj.name = data.list.resources[0].resource.fields.name;
-        obj.price = data.list.resources[0].resource.fields.price;
-        obj.volume = data.list.resources[0].resource.fields.volume;
+ 
+service.setStock = function(stock) {
+    stockName = stock;
+}
+
+service.makeUrl = function() {
+    return stockQuery +stockName +"/quote?format=json"
+}
+
+service.makeUrlYql = function() {
+    return "https://query.yahooapis.com/v1/public/yql?q=" + stockQueryYql +stockName +"%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+}
+
+service.getStockDetails = function () {
+    $http({        
+        url: service.makeUrl()
+    }).success(function(data, status, headers, config) {              
+        service.name = data.list.resources[0].resource.fields.name;
+        service.price = data.list.resources[0].resource.fields.price;
+        service.volume = data.list.resources[0].resource.fields.volume;
+    }).error(function(data, status, headers, config) {
+        service.content = data;              		
     });
-      }
+    return service;
     }
+
+service.getStockDetailsYql = function () {    
+    $http({        
+        url: service.makeUrlYql()
+    }).success(function(data, status, headers, config) {         
+        var basePath = data.query.results.quote     
+        service.name = basePath.Name;
+        service.lastTradePrice = basePath.LastTradePriceOnly;
+        service.volume = basePath.AverageDailyVolume;
+        service.daysRange = basePath.DaysRange;
+        service.yearHigh = basePath.YearHigh;
+        service.yearLow = basePath.YearLow;
+        service.daysLow = basePath.DaysLow;
+        service.daysHigh = basePath.DaysHigh;
+        //serivce.change = basePath.Change;
+        service.stockExchange = basePath.StockExchange;
+    }).error(function(data, status, headers, config) {
+        service.content = data;              		
+    });
+    return service;
+    }
+
+    return service;
 });
+
+
 
 App.factory('StockIndexResource', function($http){
     
@@ -55,6 +77,25 @@ App.factory('StockIndexResource', function($http){
            return obj;
       }
     return obj;
+}
+           
+);
+
+App.factory('StockGlobalIndexResource', function($http){
+    
+    var service = this;
+    var indexQuery = "https://query.yahooapis.com/v1/public/yql/permikkelsen/globalIndex?format=json&diagnostics=true&callback="
+    
+    service.getGlobalIndexes = function($scope) {
+        $http.get(indexQuery)
+        .success(function(data, status, headers, config) {               
+        service.value = jQuery("//results/div",data);
+            return service;
+        
+    });
+           return service;
+      }
+    return service;
 }
            
 );
